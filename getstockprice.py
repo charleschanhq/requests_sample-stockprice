922360a1143280d14078e6839659dd583ce13b97
CharlesChan <charleschanhq@gmail.com>
2017-07-10 12:06:13 -0400

    Hanqing

---
+++ getstockprice.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getHTML(url):
+    r = requests.get(url)
+    return r.content
+
+def parseHTML(html):
+    soup = BeautifulSoup(html,'html.parser')
+    body = soup.body
+    company_middle = body.find('div',attrs={'class':'middle'})
+    company_list_ct = company_middle.find('div',attrs={'class':'list-ct'})
+
+    for company_ul in company_list_ct.find_all('ul',attrs={'class':'company-list'}):
+        for company_li in company_ul.find_all('li'):
+            company_url = company_li.a['href']
+            company_info = company_li.get_text()
+            print (company_info,company_url)
+
+print('start；')
+print('company name  +  code + address')
+URL = 'http://www.cninfo.com.cn/cninfo-new/information/companylist'
+html = getHTML(URL)
+parseHTML(html)
+
+
